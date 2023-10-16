6a0bcf2aa8f6c6b55cb2299c049c92032f6e99e1
gabryelfbatista <gabryelfbatista@gmail.com>
2023-10-16 00:51:36 -0300

    corrigindo disciplina.c

--- disciplina.c
+++ disciplina.c
@@ -8,11 +8,8 @@ Disciplina *disciplina_construct(char *nome, char *codigo, char *professor)
     d->nome = nome;
     d->codigo = codigo;
     d->professor = professor;
-<<<<<<< HEAD
     d->pre_requisito = forward_list_construct();
     d->matriculas = forward_list_construct();
-=======
->>>>>>> 3b9dc3978e8da59ac3bc8c1d72a68a4a9d08caaf
     return d;
 };
 
